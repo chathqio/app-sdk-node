0c2da128553d4294eea2e576a1e3b0c7cbb2283f
hamletarencibia <hamletarencibia@gmail.com>
2023-09-18 20:34:15 -0400

    feat: add webhook management methods

--- packages/sdk-node/src/services/chathq-app-client.service.ts
+++ packages/sdk-node/src/services/chathq-app-client.service.ts
@@ -1,3 +1,8 @@
+import {
+    IWebhookBillingSubscription,
+    LivechatEventName
+} from '@chathqio/app-sdk-contracts';
+
 import { ChatHQAppClientBase } from '../base';
 import {
     AppClientOptions,
@@ -8,6 +13,8 @@ import {
 import { LIST_ENGAGMENT_WIDGETS_QUERY } from '../graphql';
 
 const ENDPOINT_DATA_GRAPH = '/graphql';
+
+const ENDPOINT_DATA_WEBHOOKS = '/webhooks';
 
 export class ChatHQAppClient extends ChatHQAppClientBase {
     //#region Constructor
@@ -59,4 +66,131 @@ export class ChatHQAppClient extends ChatHQAppClientBase {
     }
 
     //#endregion Widgets
+
+    //#region Webhooks
+    async listWebhooks(accessToken: string) {
+        try {
+            const {
+                data: { data }
+            } = await this.get<{ data: IWebhookBillingSubscription[] }>(
+                `${ENDPOINT_DATA_WEBHOOKS}/all`,
+                accessToken,
+                {}
+            );
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async deleteAllWebhooks(accessToken: string) {
+        try {
+            const {
+                data: { data }
+            } = await this.delete<{
+                data: {
+                    acknowledged: boolean;
+                    deletedCount: number;
+                };
+            }>(`${ENDPOINT_DATA_WEBHOOKS}/all`, accessToken, {});
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async createWebhook(
+        accessToken: string,
+        accountId: string,
+        event: LivechatEventName,
+        url: string
+    ) {
+        try {
+            const {
+                data: { data }
+            } = await this.post<{ data: IWebhookBillingSubscription }>(
+                ENDPOINT_DATA_WEBHOOKS,
+                accessToken,
+                {
+                    accountId,
+                    event,
+                    url
+                }
+            );
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getWebhook(
+        accessToken: string,
+        accountId: string,
+        event: LivechatEventName,
+        url: string
+    ) {
+        try {
+            const requestParams = `accountId=${accountId}&event=${event}&url=${encodeURI(
+                url
+            )}`;
+            const {
+                data: { data }
+            } = await this.get<{ data: { id: string } }>(
+                `${ENDPOINT_DATA_WEBHOOKS}?${requestParams}`,
+                accessToken
+            );
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async deleteWebhook(
+        accessToken: string,
+        accountId: string,
+        event: LivechatEventName,
+        url: string
+    ) {
+        try {
+            const {
+                data: { data }
+            } = await this.delete<{ data: IWebhookBillingSubscription }>(
+                `${ENDPOINT_DATA_WEBHOOKS}`,
+                accessToken,
+                {
+                    data: {
+                        accountId,
+                        event,
+                        url
+                    }
+                }
+            );
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getWebhookEventNames(accessToken: string) {
+        try {
+            const {
+                data: { data }
+            } = await this.get<{ data: string[] }>(
+                `${ENDPOINT_DATA_WEBHOOKS}/event-names`,
+                accessToken,
+                {}
+            );
+
+            return data;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    //#endregion Webhooks
 }

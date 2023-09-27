9353eba33a7c83d777ae2a946b930740da6e02ea
hamletarencibia <hamletarencibia@gmail.com>
2023-09-26 23:47:09 -0400

    feat: add accounts webhooks tests

--- packages/sdk-node/src/services/chathq-app-client.service.spec.ts
+++ packages/sdk-node/src/services/chathq-app-client.service.spec.ts
@@ -14,6 +14,9 @@ const SSO_TOKEN /*         */ = env.VITE_TEST_SSO_TOKEN;
 const BASE_URL /*          */ = env.VITE_TEST_BASE_URL;
 const ACCOUNT_ID /*        */ = env.VITE_TEST_ACCOUNT_ID;
 
+const TEST_WEBHOOK_SUBSCRIPTION =
+    'http://test_webhook_subscription.chathq.io.test';
+
 const OPTIONS_FROM_ENV: AppClientOptions = {
     appClientId: APP_CLIENT_ID,
     appClientSecret: APP_CLIENT_SECRET,
@@ -29,6 +32,7 @@ function serviceFactory(options: any = {}) {
 }
 
 describe(ChatHQAppClient.name, () => {
+    let webhookId = '';
     describe('constructor', () => {
         it('should succeed with options from ENV', () => {
             const service = serviceFactory()();
@@ -99,6 +103,121 @@ describe(ChatHQAppClient.name, () => {
             expectTypeOf(webhooks).toBeArray();
             expect(webhooks).toBeDefined();
             expect(webhooks).not.toBeNull();
+            webhooks.forEach(webhook => {
+                expect(webhook).toHaveProperty('_id');
+                expect(webhook).toHaveProperty('accountId');
+                expect(webhook).toHaveProperty('event');
+                expect(webhook).toHaveProperty('urls');
+                expectTypeOf(webhook.urls).toBeArray();
+            });
+        });
+    });
+
+    describe('createWebhook', () => {
+        it('should return a list of webhooks', async () => {
+            const service = serviceFactory()();
+            const ssoToken = await service.generateAccessToken(SSO_TOKEN);
+            const webhook = await service.createWebhook(
+                ssoToken.accessToken,
+                ACCOUNT_ID,
+                'LIVECHAT_SESSION',
+                `${TEST_WEBHOOK_SUBSCRIPTION}/v1`
+            );
+            expectTypeOf(webhook).toBeObject();
+            expect(webhook).toHaveProperty('_id');
+            expect(webhook).toHaveProperty('accountId');
+            expect(webhook).toHaveProperty('event');
+            expect(webhook.event).equal('LIVECHAT_SESSION');
+            expect(webhook).toHaveProperty('urls');
+            expectTypeOf(webhook.urls).toBeArray();
+            const subscriptionIndex = webhook.urls.indexOf(
+                `${TEST_WEBHOOK_SUBSCRIPTION}/v1`
+            );
+            expect(subscriptionIndex).not.equal(-1);
+            webhookId = webhook._id;
+        });
+    });
+
+    describe('getWebhook', () => {
+        it('should return a list of webhooks', async () => {
+            const service = serviceFactory()();
+            const ssoToken = await service.generateAccessToken(SSO_TOKEN);
+            const webhook = await service.getWebhook(
+                ssoToken.accessToken,
+                ACCOUNT_ID,
+                webhookId
+            );
+            expectTypeOf(webhook).toBeObject();
+            expect(webhook).toHaveProperty('_id');
+            expect(webhook).toHaveProperty('accountId');
+            expect(webhook).toHaveProperty('event');
+            expect(webhook).toHaveProperty('urls');
+            expectTypeOf(webhook.urls).toBeArray();
+        });
+    });
+
+    describe('addWebhookSubscription', () => {
+        it('should return a list of webhooks', async () => {
+            const service = serviceFactory()();
+            const ssoToken = await service.generateAccessToken(SSO_TOKEN);
+            const webhook = await service.addWebhookSubscription(
+                ssoToken.accessToken,
+                ACCOUNT_ID,
+                webhookId,
+                TEST_WEBHOOK_SUBSCRIPTION
+            );
+            expectTypeOf(webhook).toBeObject();
+            expect(webhook).toHaveProperty('_id');
+            expect(webhook).toHaveProperty('accountId');
+            expect(webhook).toHaveProperty('event');
+            expect(webhook).toHaveProperty('urls');
+            expectTypeOf(webhook.urls).toBeArray();
+            const subscriptionIndex = webhook.urls.indexOf(
+                TEST_WEBHOOK_SUBSCRIPTION
+            );
+            expect(subscriptionIndex).not.equal(-1);
+        });
+    });
+
+    describe('removeWebhookSubscription', () => {
+        it('should return a list of webhooks', async () => {
+            const service = serviceFactory()();
+            const ssoToken = await service.generateAccessToken(SSO_TOKEN);
+            const webhook = await service.removeWebhookSubscription(
+                ssoToken.accessToken,
+                ACCOUNT_ID,
+                webhookId,
+                TEST_WEBHOOK_SUBSCRIPTION
+            );
+            expectTypeOf(webhook).toBeObject();
+            expect(webhook).toHaveProperty('_id');
+            expect(webhook).toHaveProperty('accountId');
+            expect(webhook).toHaveProperty('event');
+            expect(webhook).toHaveProperty('urls');
+            expectTypeOf(webhook.urls).toBeArray();
+            const subscriptionIndex = webhook.urls.indexOf(
+                TEST_WEBHOOK_SUBSCRIPTION
+            );
+            expect(subscriptionIndex).equal(-1);
+        });
+    });
+
+    describe('deleteWebhook', () => {
+        it('should return a list of webhooks', async () => {
+            const service = serviceFactory()();
+            const ssoToken = await service.generateAccessToken(SSO_TOKEN);
+            const webhook = await service.deleteWebhook(
+                ssoToken.accessToken,
+                ACCOUNT_ID,
+                webhookId
+            );
+            expect(webhook).toHaveProperty('_id');
+            expect(webhook._id).equal(webhookId);
+            expect(webhook).toHaveProperty('accountId');
+            expect(webhook).toHaveProperty('event');
+            expect(webhook.event).equal('LIVECHAT_SESSION');
+            expect(webhook).toHaveProperty('urls');
+            expectTypeOf(webhook.urls).toBeArray();
         });
     });
 

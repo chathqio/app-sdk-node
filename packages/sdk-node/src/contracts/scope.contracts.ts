2bf51c4d7e766309a5ceeb345a1b49c0aea71116
Sergio Leon <8313760+cbnsndwch@users.noreply.github.com>
2023-09-29 12:44:33 -0400

    feat: add webhooks API scopes

--- packages/sdk-node/src/contracts/scope.contracts.ts
+++ packages/sdk-node/src/contracts/scope.contracts.ts
@@ -1,5 +1,14 @@
 export const SCOPE_WIDGETS_READ_ONLY = 'widgets:read';
+export const SCOPE_WEBHOOKS_READ_ONLY = 'wwebhooks:read';
+export const SCOPE_WEBHOOKS_WRITE = 'wwebhooks:read';
 
-export type ChatHQAppClientScope = typeof SCOPE_WIDGETS_READ_ONLY;
+export type ChatHQAppClientScope =
+    | typeof SCOPE_WIDGETS_READ_ONLY
+    | typeof SCOPE_WEBHOOKS_READ_ONLY
+    | typeof SCOPE_WEBHOOKS_WRITE;
 
-export const APP_SCOPES_ALL: ChatHQAppClientScope[] = [SCOPE_WIDGETS_READ_ONLY];
+export const APP_SCOPES_ALL: ChatHQAppClientScope[] = [
+    SCOPE_WIDGETS_READ_ONLY,
+    SCOPE_WEBHOOKS_READ_ONLY,
+    SCOPE_WEBHOOKS_WRITE
+];
